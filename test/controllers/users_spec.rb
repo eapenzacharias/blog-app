b87efe192a87f2d91ef4cde2a90d4900d7bbabf7
eapenzacharias <eapenzac@gmail.com>
2022-03-02 23:13:06 +0100

    added test for user show

--- test/controllers/users_spec.rb
+++ test/controllers/users_spec.rb
@@ -12,4 +12,13 @@ RSpec.describe 'Users', type: :request do
       expect(response.body).to render_template(:index)
     end
   end
+  describe 'GET /show' do
+    before(:example) { get '/users/sample_user'}
+    it 'Should have http status ok' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'should render user index page' do
+      expect(response.body).to include('User: sample_user')
+    end
+  end
 end
