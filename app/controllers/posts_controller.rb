8711712dad70632a5896f3b41805c240ae09a1a4
eapenzacharias <eapenzac@gmail.com>
2022-03-02 23:08:15 +0100

    added posts view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,8 @@
 class PostsController < ApplicationController
   def index; end
 
-  def show; end
+  def show
+    @user_id = params[:user_id]
+    @post_id = params[:id]
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,6 @@ class UsersController < ApplicationController
   def index; end
 
   def show
-    @id = params[:id]
+    @user_id = params[:id]
   end
 end
