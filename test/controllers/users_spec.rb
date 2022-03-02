45a40ff962aa2d12d73380fec4899f0dc57429db
eapenzacharias <eapenzac@gmail.com>
2022-03-02 22:53:43 +0100

    added tests for user index page

---
+++ test/controllers/users_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
+    before(:example) { get '/users'}
+    it 'Should have http status ok' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'should render user index page' do
+      expect(response.body).to render_template(:index)
+    end
+  end
+end
