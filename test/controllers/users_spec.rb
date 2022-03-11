7efae4f97b867c213f57250354d704de0da22ad7
eapenzacharias <eapenzac@gmail.com>
2022-03-11 19:52:31 +0100

    updated controller specs

--- test/controllers/users_spec.rb
+++ test/controllers/users_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
+    User.create(name: 'user1', photo: 'photo.jpg', bio: 'bio')
     before(:example) { get '/users' }
     it 'Should have http status ok' do
       expect(response).to have_http_status(:ok)
@@ -13,13 +14,13 @@ RSpec.describe 'Users', type: :request do
     end
   end
   describe 'GET /show' do
-    before(:example) { get '/users/sample_user' }
+    before(:example) { get '/users/1' }
     it 'Should have http status ok' do
       expect(response).to have_http_status(:ok)
     end
     it 'should render user index page' do
       expect(response.body).to render_template(:show)
-      expect(response.body).to include('User: sample_user')
+      expect(response.body).to include('user1')
     end
   end
 end
