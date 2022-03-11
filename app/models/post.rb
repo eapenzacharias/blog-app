2260e6a303b0ee325689206bd135048ed39d5be1
eapenzacharias <eapenzac@gmail.com>
2022-03-11 20:44:11 +0100

    added test for `post`model

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,13 +11,13 @@ class Post < ApplicationRecord
 
   after_update :update_posts_counter
 
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
   private
 
   def update_posts_counter
     author.update(posts_counter: author.posts.length)
-  end
-
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
   end
 end
