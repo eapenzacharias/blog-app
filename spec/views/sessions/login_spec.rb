366aad853810df9e83d2d4b8dad9919ff5f2b258
eapenzacharias <eapenzac@gmail.com>
2022-03-16 22:23:51 +0100

    Added tests for User#index

--- spec/views/sessions/login_spec.rb
+++ spec/views/sessions/login_spec.rb
@@ -3,9 +3,10 @@
 require 'rails_helper'
 require 'capybara/rspec'
 
-RSpec.describe 'Login' do
+RSpec.describe 'Login' do # rubocop:disable Metrics/BlockLength
   before :each do
-    User.create(name: 'User', email: 'user@test.com', password: '123456789', password_confirmation: '123456789', confirmed_at: Time.now)
+    User.create(name: 'User', email: 'user@test.com', password: '123456789', password_confirmation: '123456789',
+                confirmed_at: Time.now)
   end
 
   describe 'When visiting log in page', type: :feature do
