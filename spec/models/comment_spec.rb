08442add435d7eadcb2fb5dd91233f71edc17bcb
eapenzacharias <eapenzac@gmail.com>
2022-03-16 17:56:51 +0100

    Updated all tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,5 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create(id: 1, name: 'User', email: 'test65@test.com', password: 'test1234', password_confirmation: 'test1234', confirmed_at: Time.now)
+    @post = Post.create(id: 1, title: 'Post', author_id: @user.id, text: 'post content')
+  end
+
+  it 'should increment comments_counter' do
+    Comment.create(text: 'first comment', author_id: 1, post_id: 1)
+    expect(Post.find(1).comments_counter).to eq 1
+  end
 end
